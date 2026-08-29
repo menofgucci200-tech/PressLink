5a226b2d919717fa4630e58b1215821a27238d91
LKM Digital <famillelasme@gmail.com>
2026-08-29 12:06:12 +0000

    Programme le traitement de la queue via cron (hébergement mutualisé sans worker persistant)

--- presslink-api/routes/console.php
+++ presslink-api/routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Hébergement mutualisé sans worker persistant : on vide la queue via cron,
+// par tranches de 50s max, toutes les minutes (voir routes/console.php + cron LWS).
+Schedule::command('queue:work --stop-when-empty --max-time=50 --tries=3')
+    ->everyMinute()
+    ->withoutOverlapping();
